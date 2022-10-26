86a09a66e90c8f74c2e64fe01c98931290114a84
fmirmuln <sabbirbeb@mail.ru>
2022-10-26 15:55:08 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Realtime_chat',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "Django~=4.0.1",
+        "channels~=3.0.4"
+    ],
+    entry_points='''
+
+    ''',
+)
